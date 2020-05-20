f914979e15ac052a23cd438a4523569e7a88a8ff
André Filipe <andre.filipe.l@tecnico.ulisboa.pt>
2020-05-20 02:50:55 +0100

    Update proj.lisp

--- proj.lisp
+++ proj.lisp
@@ -1,7 +1,7 @@
 (in-package :user)
 
-
 ;;https://github.com/srps/SameGame/blob/master/g012.lisp
+
 (defvar *array_size_col* )
 (defvar *array_size_lin* )
 (defvar *board_aux* )
@@ -18,8 +18,7 @@
 			(setq rest_list (append rest_list (list (cdr li))))
 
 		)
-		
-		
+				
 		(setq form_list (append  form_list (list new_list) ))
 		(setq board rest_list )
 		(setq new_list nil)
@@ -28,6 +27,7 @@
 	(format t " ~a ~%" form_list)
 	(return-from transpose-board form_list)
 )
+
 
 (defun compactar-horizontal (form_list)
 	(setq zero_list nil)
@@ -46,9 +46,10 @@
 		(setq non_zero_list nil)
 
 	)
+	; Era o nome das funções que estava trocada ou a string embaixo?
 	(format t " Compacta vertical ~a ~%" total_list )	
 	(return-from compactar-horizontal total_list)
-	)
+)
 
 
 (defun compactar-vertical (total_list)
@@ -57,13 +58,11 @@
 				(setq zero_list  (append zero_list (list lista)))
 				(setq non_zero_list (append non_zero_list (list lista)))
 			)
-
 	)
-
 	(setq total_list (append non_zero_list zero_list  )) 
 	(format t " compacta horizontal : ~a ~%" total_list )
 	(return-from compactar-vertical total_list)
-	)
+)
 
 
 (defun compactar-tabuleiro (board)
@@ -76,22 +75,17 @@
 	(return-from compactar-tabuleiro transpose_list)
 )
 
-
-
-(setq new ( transpose-board '((1 0 4 0) (0 0 4 0) (3 0 4 2))))
-(setq new2 (compactar-horizontal new))
-(setq new3 (compactar-vertical new2))
-(setq new2 (transpose-board new3))
-
-
+;(setq new ( transpose-board '((1 0 4 0) (0 0 4 0) (3 0 4 2))))
+;(setq new2 (compactar-horizontal new))
+;(setq new3 (compactar-vertical new2))
+;(setq new2 (transpose-board new3))
 
 (defun lista-convert-to-array (lista)
 	(setf state (make-array '(4 4) 
    		:initial-contents lista)
-	)
-    
+	)    
     state
-    )
+)
 
 
 (defun list_set_limits_size (board)
@@ -99,7 +93,8 @@
 	(setq  *array_size_lin* (list-length board ))
 	(format t " ~a ~%"  *array_size_col* )
 	(format t " ~a ~%"  *array_size_lin* )
-	)
+)
+
 
 (defun create-auxiliar-board ()
 	(setq *board_aux* (make-array (list *array_size_lin* *array_size_col*))) 
@@ -108,25 +103,26 @@
   				(setf (aref *board_aux* i j) 0 )
 			)
 	)
-	(return-from create-auxiliar-board *board_aux*)
-    	
+	(return-from create-auxiliar-board *board_aux*)    	
 )
 
 
 (defun get_value (posx posy array_board)
 	(setq value (aref array_board posx posy))
 	value
-	)
+)
+
 
 (defun fill_value  (posx posy array_board value) 
 	(setf (aref array_board posx posy) value )
 	array_board
-	)
+)
+
 
 (defun list_is_member (lista linha coluna)
 	(return-from  list_is_member (find (list linha coluna) lista :test #'equal))
+)
 
-	)
 
 (defun find_color_cluster_positions (posx posy array_board color list-neighbors ) 
 	(if (and (>= posx 0) (and (>= posy 0) (and (< posx *array_size_lin*) (< posy *array_size_col*))))
@@ -187,20 +183,14 @@
 					) 
 				)
 			)
-
-
 			(if (= (list-length list-neighbors) 0 )
 				(setq list-neighbors (append  list-neighbors (list posx posy ) ))
 			)
-			
 			;(format t " estou aqui3 ~%"  )
 		)
 	)
 	list-neighbors
 )
-
-
-
 
 
 (defun find_color_blocks (board)
@@ -227,15 +217,100 @@
 			)
 		)
 	)
-empList	
+	empList	
 )
 
-;(trace find_color_blocks )
-;(trace find_color_cluster_positions)
-;(trace list_is_member)
-;(trace fill_value)
+
+(defun board_remove_group (board group)
+	(setq newBoard nil)
+	(loop for line in board do
+		(setq copy_list (copy-tree line))		; function copy-list if we intend to preserve the elements
+		(setq newBoard (append newBoard (list copy_list)))
+	)
+	; alternatively (setq newBoard (copy-tree board))
+
+	(loop for el in group do
+		(setq l (car el))
+		(setq c (cadr el))
+		(setf (nth c (nth l newBoard)) 0)
+	)
+	(setq newBoard (compactar-tabuleiro newBoard))
+	newBoard
+)
 
 
+(defclass SGState()
+	((_board
+		:initarg :board
+		:accessor SGState-board)
+	)
+)
+
+(defmethod lessThan ((o1 SGState) (o2 SGState))
+	(< (list-length (SGState-board o1)) (list-length (SGState-board o2)) )
+)
+
+
+(defclass SameGame()	; FIXME inherits from Problem
+	((_board :initarg :board :accessor SameGame-board)
+	 (_initial :accessor SameGame-initial))
+)
+
+(defmethod initialize-instance :after ((o SameGame) &key)
+	(setf (SameGame-initial o) (make-instance 'SGState :board (SameGame-board o)))
+)
+; Este constructor todo só para ter:
+; class same_game(Problem):
+; 	def __init__(self, b):
+; 		self.board = b
+;		self.initial = SGState(b)
+
+(defmethod actions ((s  SGState))
+	(setf possible_actions (board_find_groups (SGState-board s)))
+	(loop for a in possible_actions do
+		(if (> (list-length a) 1)
+			(setq actions (append actions (list a)) )
+		)
+	)
+	actions
+)
+
+(defmethod goal_test ((s SGState))
+	(setq idx_last_line (- (list-length (SGState-board s)) 1))
+	(setq line_board (nth idx_last_line SGState-board s))
+	(loop for pos in line_board do
+		(if (/= pos 0)
+			(return-from goal_test nil)
+		)
+	)
+	t
+)
+
+(defmethod path_cost ((c integer) (s1 SGState) (s2 SGState) (action list))
+	(+ c 1)
+)
+
+(defmethod result ((s SGState) (action list))
+	(setq b (board_remove_group (SGState-board s) action))
+	(make-instance 'SGState :board b)
+)
+
+;(defparameter s1 (make-instance 'SGState :board '((1 0 4 0) (0 0 4 0) (3 0 4 0) (0 0 1 0)) ))
+;(defparameter sg1 (make-instance 'SameGame :board '((1 0 4 0) (0 0 4 0) (3 0 4 0) (0 0 1 0)) ))
+;(setq remove_aux '((0 0) (0 2) (2 0)))
+;(write (SGState-board (result (SameGame-initial sg1) remove_aux))) (terpri)
+
+
+;; ----------------------------------------------------------------------------------------------------------
+
+
+(defun 2d-array-to-list (array)
+  (setf lisa (loop for i below (array-dimension array 0)
+        collect (loop for j below (array-dimension array 1)
+                      collect (aref array i j))))
+  
+  (return-from 2d-array-to-list  lisa)
+)
 
 ;;def board_find_groups(board):
 ;;    i =0
@@ -257,58 +332,21 @@ empList
 ;    #print(groups_total)
 ;    return groups_total
 
-(format t " ~a ~%"  :(- 3 1))
 
-(setq board '((1 0 4 0) (0 0 4 0) (3 0 4 0) (0 0 1 0)) )
-(setq board_comp (compactar-tabuleiro board))
-(list_set_limits_size board )
-( create-auxiliar-board)
+;(format t " ~a ~%"  :(- 3 1))
 
-(format t " ~a ~%"  (find_color_blocks board))
+;(setq board_comp (compactar-tabuleiro board))
+;(list_set_limits_size board)
+;(create-auxiliar-board)
 
-;;(setq board_comp (compactar-tabuleiro (lista-convert-to-array  board_comp )))
+;(format t " ~a ~%"  (find_color_blocks board))
 
-(defun 2d-array-to-list (array)
-  (setf lisa (loop for i below (array-dimension array 0)
-        collect (loop for j below (array-dimension array 1)
-                      collect (aref array i j))))
-  
-  (return-from 2d-array-to-list  lisa)
-)
-
+;(setq board_comp (compactar-tabuleiro (lista-convert-to-array  board_comp )))
 
 ;;(list_set_limits_size '( )
 
-(setq array (lista-convert-to-array board))
+;(setq array (lista-convert-to-array board))
 
-(format t " ~a ~%"  (2d-array-to-list array))
-
-
-
-
-;;
-;;def board_remove_group(board, group):
-;;
-;;    copy_board = []
-;;    i = 0
-;;    j = 0
-;;    for i in range(len(board)):
- ;;       copy_list = board[i][:]
-;;        copy_board.append(copy_list)
-;;    for j in range(len(group)):
-;;        pos = group[j]
-;;        
-;;        line = pos_l(pos)
-;;        column = pos_c(pos)
-;;        copy_board[line][column] = 0
-;;
-;;    
-;;    copy_board = vertical_compact(copy_board)
-;;    copy_board = horizontal_compact(copy_board)
-;;    return copy_board
-
-
+;(format t " ~a ~%"  (2d-array-to-list array))
 
 ;;(compactar-horizontal '((1 2 2 3 3) (2 2 2 1 3) (1 2 2 2 2) (1 1 1 1 1)))
-
-;;
